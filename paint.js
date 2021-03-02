19ba0bc182e7bfc2e9862cdd641ef13ee5d0bb74
Sal <salvaguti2019@gmail.com>
2021-03-01 17:12:10 -0800

    Working on brush selection

--- paint.js
+++ paint.js
@@ -1,13 +1,22 @@
+//Global Variables
+var currentBrush = "brush";
+var strokeSize = 1;
 
 function setup(){
     createCanvas(1100, 700);
     background(255);
     noStroke();
 }
- 
-function draw(){ 
 
-    // Sidebar
+function draw(){
+    drawSidebar();
+}
+
+function drawSidebar(){
     fill(40);
     rect(0, 0, 100, 700);
+}
+
+function mousePressed(){
+    
 }
