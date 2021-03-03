b09b305c144f00500e467633fc39a058af5d66e7
Diego Garay <dcgaray01@gmail.com>
2021-03-02 21:58:51 -0800

    added a basic text box
    Okay, we're getting the ball rolling!

--- paint.js
+++ paint.js
@@ -5,7 +5,7 @@
  *LINK TO THE VIDEO: https://www.youtube.com/watch?v=S1TQCi9axzg
  */
 
-class Letter {
+ class Letter {
   constructor(x, y, d) {
     this.char = Letter.getChar()
     this.x = x
@@ -131,6 +131,7 @@ var currentStroke = 100;
 var buttons = []; // Used for buttons and other interactions
 var canvas;
 var shapes = [];
+var input;
 
 var bps = [25, 125, 225, 325]
 
@@ -148,6 +149,8 @@ function setup() {
 
   canvas = new Hitbox(120, 0, window.innerWidth - 175, window.innerHeight - 195);
 
+  createP('');
+  input = createInput('');
   //canvas.position(50, 100);
   //canvas.position(10, 10, 'fixed')
   //noStroke()
@@ -166,11 +169,18 @@ function draw() {
       shapes[i].draw();
     }
     sidebar();
+<<<<<<< Updated upstream
 <<<<<<< HEAD
     colorPicker(10,280);
 =======
     colorPicker(10,410);
 >>>>>>> cb1dd151cc1606abbd4029f57c9dac7b9c6f48c3
+=======
+    colorPicker(10,20);
+
+    fill(0);
+    text(input.value(), 500, 500);
+>>>>>>> Stashed changes
   //rain.forEach(s => s.draw())
 }
 
@@ -178,9 +188,9 @@ function draw() {
  * in which the user will be able to change the color
  * they're using, the paint brush, etc....
  */
-function sidebar(){
+ function sidebar(){
     // Sidebar
-  noStroke();
+    noStroke();
   fill(25); // change 25
   rect(0, 0, 120, window.innerHeight);
 
@@ -262,11 +272,11 @@ function colorPicker(x,y){
         b = (255/100) * (sx-10);
       }
     }
-}
+  }
 
-function sizePicker(x, y){}
+  function sizePicker(x, y){}
 
-function mousePressed(){
+  function mousePressed(){
 
   // Check if a brush button was pressed
   for(var i = 0; i < buttons.length; i++){
@@ -307,9 +317,9 @@ class Hitbox{
 
   check(mX, mY){ //This will return true or false if mouse was clicked on top of the hitbox
     if(mX >= this.x && mX <= this.x + this.w && mY >= this.y && mY <= this.y + this.h && this.active)
-        return true;
+      return true;
     else
-        return false;
+      return false;
   }
 }
 
